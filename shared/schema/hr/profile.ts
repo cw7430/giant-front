d9a4534e92679d04d80ad4477b22d3839d3d80ec
cw7430 <cw282@naver.com>
2026-03-22 22:12:16 +0900

    스키마 세팅

---
+++ shared/schema/hr/profile.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+
+import { zStringToDate } from '~~/shared/utils/zod-helper';
+import { pageRequestSchema, pageResponseSchema } from '~~/shared/schema/base/page';
+
+export const employeeProfilesRequestSchema = pageRequestSchema([
+  'employee',
+  'position',
+  'department',
+] as const);
+
+export const employeeProfileResponseSchema = z.object({
+  employeeId: z.string(),
+  employeeCode: z.string(),
+  employeeRole: z.enum(['DEPARTMENT_CHIEF', 'TEAM_CHIEF', 'EMPLOYEE', 'LEFT']),
+  employeeName: z.string(),
+  positionCode: z.string(),
+  positionName: z.string(),
+  departmentCode: z.string(),
+  departmentName: z.string(),
+  teamCode: z.string(),
+  teamName: z.string(),
+  phoneNumber: z.string(),
+  email: z.string(),
+  createdBy: z.string().nullable(),
+  createdEmployeeName: z.string().nullable(),
+  updatedBy: z.string().nullable(),
+  updatedEmployeeName: z.string().nullable(),
+  createdAt: zStringToDate,
+  updatedAt: zStringToDate,
+  leftAt: z.date().nullable(),
+});
+
+export const employeeProfilesResponseSchema = pageResponseSchema(
+  employeeProfileResponseSchema,
+);
+
+export type EmployeeProfilesRequestDto = z.infer<
+  typeof employeeProfilesRequestSchema
+>;
+
+export type EmployeeProfileResponseDto = z.infer<
+  typeof employeeProfileResponseSchema
+>;
+
+export type EmployeeProfilesResponseDto = z.infer<
+  typeof employeeProfilesResponseSchema
+>;
