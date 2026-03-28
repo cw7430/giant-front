6e661f5776ab53c7d4a22173950c4b204182e53a
cw7430 <cw282@naver.com>
2026-03-28 17:39:39 +0900

    직원 프로파일 서버 구현중

---
+++ layers/base/app/components/layouts/CustomPagination.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { PageResponseMetaDto } from '~~/layers/base/shared/schema/page';
+
+const props = defineProps<{
+  data: PageResponseMetaDto;
+}>();
+
+const {
+  size,
+  // totalElements,
+  totalPages,
+  currentPage,
+  // startPage,
+  // endPage,
+  // hasNext,
+  // hasPrevious,
+} = props.data;
+</script>
+
+<template>
+  <BPagination
+    v-model="currentPage"
+    :total-rows="totalPages"
+    :per-page="size"
+    first-text="First"
+    prev-text="Prev"
+    next-text="Next"
+    last-text="Last"
+  />
+</template>
--- layers/base/nuxt.config.ts
+++ layers/base/nuxt.config.ts
@@ -47,12 +47,12 @@ export default defineNuxtConfig({
     ],
   },
 
-  // components: [
-  //   {
-  //     path: '~~/layers/base/app/components',
-  //     pathPrefix: false,
-  //   },
-  // ],
+  components: [
+    {
+      path: '~~/layers/base/app/components',
+      pathPrefix: false,
+    },
+  ],
 
   app: {
     head: {
